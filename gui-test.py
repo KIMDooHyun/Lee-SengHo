660dd2f4a8f5b05991c981f2b33bb49bbe74297f
Lee Seong Ho <ish1535@gmail.com>
2017-06-02 21:45:02 +0900

    Update gui-test.py

--- gui-test.py
+++ gui-test.py
@@ -1,7 +1,11 @@
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote, quote_plus, unquote
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
 import xml.etree.ElementTree as ET
 import webbrowser
+import mimetypes
+import mysmtplib
 from appJar import gui
 
 app=gui()
@@ -27,6 +31,11 @@ def searching(rb):
             url = baseurl+queryParams
             print(url)
             def main():
+                global ITEM_NAME
+                global ITEM_SEQ
+                global ENTP_NAME
+                global CLASS_NAME
+                global ImageUrl
                 data=urlopen(url).read() 
                 #print(data)
                 f=open("drug-db-itemname.xml","wb")
@@ -41,6 +50,7 @@ def searching(rb):
                     CLASS_NAME = root.findtext('body/items/item/CLASS_NAME')
                     ITEM_SEQ = root.find('body/items/item/ITEM_SEQ').text
                     ENTP_NAME = root.findtext('body/items/item/ENTP_NAME')
+                    ImageUrl = root.findtext('body/items/item/ITEM_IMAGE')
                     app.setLabel("등록번호-1", "약물등록번호 : ")
                     app.setLabel("등록번호-2", ITEM_SEQ)
                     app.setLabel("이름-1", "약물명 : ")
@@ -61,6 +71,10 @@ def searching(rb):
             url = baseurl+queryParams
             print(url)
             def main():
+                global PRMS_DT
+                global PRDLST_NM
+                global BSSH_NM
+                global NTK_MTHD
                 data=urlopen(url).read()
                 #print(data)
                 f=open("htfs-db-itemname.xml","wb")
@@ -96,6 +110,9 @@ def searching(rb):
             url = baseurl + queryParams
             print(url)
             def main():
+                global ITEM_NAME
+                global period_NAME
+                global inflist
                 data = urlopen(url).read()
                 f = open('MdcinSdefct-db.xml','wb')
                 f.write(data)
@@ -108,7 +125,7 @@ def searching(rb):
                     app.errorBox("경고","이름을 다시한번 확인해 주십시오.")
                 else:
                     period_NAME = root.findtext('body/items/item/COL_004')
-                    app.setLabel("등록번호-1", "")
+                    app.setLabel("등록번호-1", '')
                     app.setLabel("등록번호-2", '')
                     app.setLabel("이름-1", "약품명 : ")
                     app.setLabel("이름-2", ITEM_NAME)
@@ -136,30 +153,76 @@ def imagedownload(rb):
         app.errorBox("경고", "검색창에 약물을 입력해주세요.")
     else:
         if app.getRadioButton("drug")=='약품 이름검색':
-            itemname=app.getEntry("searching_option_item")
-            decode_key = unquote('Bw24TtXAIcROPn%2FcAPiatMkvhPRC6KbKXX%2BIaV%2FVN5fy3GgNB8Tj92PS6FNoHDb1GV2W2v%2FtZT4HvX9x3SmWmA%3D%3D')
-            baseurl = 'http://apis.data.go.kr/1470000/MdcinGrnIdntfcInfoService/getMdcinGrnIdntfcInfoList'
-            queryParams = '?' + urlencode({ quote_plus('ServiceKey') : decode_key, quote_plus('item_name') : itemname, quote_plus('entp_name') : '', quote_plus('pageNo') : '1', quote_plus('numOfRows') : '1' })
-            url = baseurl+queryParams
-            def main():
-                data=urlopen(url).read() 
-                #print(data)
-                f=open("drug-db-itemname.xml","wb")
-                f.write(data)
-                f.close()
-                tree = ET.parse('drug-db-itemname.xml')
-                root=tree.getroot()                
-                ImageUrl = root.findtext('body/items/item/ITEM_IMAGE')
-                if ImageUrl==None:
-                    app.errorBox("경고","이름을 다시한번 확인해 주십시오.")
-                else:
-                    print(ImageUrl)
-                    webbrowser.open_new(ImageUrl)
-            main()
+            searching(rb)
+            if ImageUrl==None:
+                app.errorBox("경고","이름을 다시한번 확인해 주십시오.")
+            else:
+                print(ImageUrl)
+                webbrowser.open_new(ImageUrl)
         else:
             app.errorBox("경고", "약품 이름 검색으로만 가능한 서비스입니다.")
-def sendemail(rb):
-    print("이메일전송부분")
+def sendemailbutton(rb):
+    if app.getRadioButton("drug")=='약품 이름검색':
+        if app.getLabel("이름-1")=='':
+            app.errorBox("경고","먼저 검색을 하셔야합니다.")
+        else:
+            senderAddr = "drugdbkpu@gmail.com"
+            recipientAddr=app.textBox("email 주소 입력", "이메일 주소를 입력해주세요.")
+            host = "smtp.gmail.com"
+            port = "587"
+            text = "약물 등록번호 : %s \n약물명 : %s \n제조사 : %s \n용도 : %s \n이미지 링크 : %s" %(ITEM_SEQ, ITEM_NAME, ENTP_NAME, CLASS_NAME, ImageUrl)
+            msg = MIMEText(text)
+            msg['To'] = senderAddr
+            msg['From'] = recipientAddr
+            msg['Subject'] = '약물 정보 공개'
+            s = mysmtplib.MySMTP(host,port)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login("drugdbkpu@gmail.com","20152100511!")
+            s.sendmail(senderAddr , [recipientAddr], msg.as_string())
+            app.infoBox("전송","전송되었습니다.")
+            s.close()
+    if app.getRadioButton("drug")=='건강 식품검색':
+        if app.getLabel("이름-1")=='':
+            app.errorBox("경고","먼저 검색을 하셔야합니다.")
+        else:
+            senderAddr = "drugdbkpu@gmail.com"
+            recipientAddr=app.textBox("email 주소 입력", "이메일 주소를 입력해주세요.")
+            host = "smtp.gmail.com"
+            port = "587"
+            text = "식품 등록번호 : %s \n식품명 : %s \n제조사 : %s \n복용 방법 : %s" %(PRMS_DT, PRDLST_NM, BSSH_NM, NTK_MTHD)
+            msg = MIMEText(text)
+            msg['To'] = senderAddr
+            msg['From'] = recipientAddr
+            msg['Subject'] = '건강보조식품 정보 공개'
+            s = mysmtplib.MySMTP(host,port)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login("drugdbkpu@gmail.com","20152100511!")
+            s.sendmail(senderAddr , [recipientAddr], msg.as_string())
+            app.infoBox("전송","전송되었습니다.")
+    if app.getRadioButton("drug")=='부작용보고 약물검색':
+        if app.getLabel("이름-1")=='':
+            app.errorBox("경고","먼저 검색을 하셔야합니다.")
+        else:
+            senderAddr = "drugdbkpu@gmail.com"
+            recipientAddr=app.textBox("email 주소 입력", "이메일 주소를 입력해주세요.")
+            host = "smtp.gmail.com"
+            port = "587"
+            text = "약물명 : %s \n약물 허용 기간 : %s \n증상 : %s" %(ITEM_NAME, period_NAME, inflist)
+            msg = MIMEText(text)
+            msg['To'] = senderAddr
+            msg['From'] = recipientAddr
+            msg['Subject'] = '부작용 약물 정보 공개'
+            s = mysmtplib.MySMTP(host,port)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login("drugdbkpu@gmail.com","20152100511!")
+            s.sendmail(senderAddr , [recipientAddr], msg.as_string())
+            app.infoBox("전송","전송되었습니다.")
 #이후 구현할 기능 : 검색 후 바로 메일 전송
 app.addEntry("searching_option_item",0,0)
 app.addButton("검색",searching,0,1)
@@ -167,17 +230,17 @@ app.setEntryDefault("searching_option_item", "　")
 app.addLabel("f2", "검색할 환경을 선택하십시오.",2,0)
 app.addButton("설정", setting,2,1)
 app.addRadioButton("drug","약품 이름검색",3)
-app.addRadioButton("drug","건강 식품검색",4)
+app.addRadioButton("drug","건강 식품검색",4) 
 app.addRadioButton("drug","부작용보고 약물검색",5)
-app.addLabel("등록번호-1", "　",8,0)
-app.addLabel("등록번호-2", "　",8,2)
-app.addLabel("이름-1", "　",9,0)
-app.addLabel("이름-2", "　",9,2)
-app.addLabel("제조사/증상-1", "　",10,0)
-app.addLabel("제조사/증상-2", "　",10,2)
-app.addLabel("용도/복용방법-1", "　",11,0)
-app.addLabel("용도/복용방법-2", "　",11,2)
-app.addButton("약품 이미지 다운로드",imagedownload,12,1)
-app.addButton("금지약물 위키피디아",openwiki,13,1)
-app.addButton("이메일전송",sendemail,14,1)
+app.addLabel("등록번호-1", None,8,0)
+app.addLabel("등록번호-2", None,8,1)
+app.addLabel("이름-1", None,9,0)
+app.addLabel("이름-2", None,9,1)
+app.addLabel("제조사/증상-1",None,10,0)
+app.addLabel("제조사/증상-2",None,10,1)
+app.addLabel("용도/복용방법-1",None,11,0)
+app.addLabel("용도/복용방법-2",None,11,1)
+app.addButton("약품 이미지 다운로드",imagedownload,12,0)
+app.addButton("금지약물 위키피디아",openwiki,13,0)
+app.addButton("이메일전송",sendemailbutton,14,0)
 app.go()
